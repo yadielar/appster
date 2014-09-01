78be5181f5fac1459ef79d37bbe6c6d6561156c4
Yadiel Arroyo <yadielar@gmail.com>
2014-09-01 13:36:33 -0400

    Fix for blank view that appears when visitng "/apps-de-la-semana".

--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,7 @@ angular.module('Appster', [
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 	//
 	// For any unmatched url, redirect to /state1
-	$urlRouterProvider.otherwise("/apps-de-la-semana");
+	$urlRouterProvider.otherwise("/apps-de-la-semana/");
 	//
 	// Now set up the states
 	$stateProvider
