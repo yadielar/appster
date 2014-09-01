0f57e19f555da363378d59096ca6d63a44b8505d
Yadiel Arroyo <yadielar@gmail.com>
2014-08-31 22:34:10 -0400

    Get app data from playmarketdata.json.

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -1,5 +1,8 @@
 angular.module('Appster.controllers.Main', [])
 
-.controller('MainController', ['$scope', function($scope){
-  
+.controller('MainController', ['$scope', '$http', function($scope, $http){
+  $scope.appsOfTheWeek = [];
+  $http.get('/data/playmarketdata.json').success(function(data){
+  	$scope.appsOfTheWeek = data.results;
+  });
 }]);
